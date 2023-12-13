47db23c7e2a9db33d0136c4255865d0cb8d3cd54
Liam Jackson <liamjackson126@gmail.com>
2023-12-13 13:59:00 -0500

    * nala install script installs to Downloads folder

--- notes/nala_install.sh
+++ notes/nala_install.sh
@@ -9,11 +9,13 @@
 
 DOWNLOADS="${HOME}/Downloads"
 
-curl "https://gitlab.com/volian/volian-archive/uploads/b20bd8237a9b20f5a82f461ed0704ad4/volian-archive-keyring_0.1.0_all.deb" --output "${DOWNLOADS}"
-sha256sum "${DOWNLOADS}/volian-archive-keyring_0.1.0_all.deb"
+curl -O "https://gitlab.com/volian/volian-archive/uploads/b20bd8237a9b20f5a82f461ed0704ad4/volian-archive-keyring_0.1.0_all.deb" --output-dir "${DOWNLOADS}"
+echo "SHA256 checksum:"
+sha256sum "${DOWNLOADS}/volian-archive-keyring_0.1.0_all.deb" | awk '{print $1}'
 
-curl "https://gitlab.com/volian/volian-archive/uploads/d6b3a118de5384a0be2462905f7e4301/volian-archive-nala_0.1.0_all.deb" --output "${DOWNLOADS}"
-sha256sum "${DOWNLOADS}/volian-archive-nala_0.1.0_all.deb"
+curl -O "https://gitlab.com/volian/volian-archive/uploads/d6b3a118de5384a0be2462905f7e4301/volian-archive-nala_0.1.0_all.deb" --output-dir "${DOWNLOADS}"
+echo "SHA256 checksum:"
+sha256sum "${DOWNLOADS}/volian-archive-nala_0.1.0_all.deb" | awk '{print $1}'
 
 sudo apt install "${DOWNLOADS}/volian-archive-*.deb"
 echo "deb-src https://deb.volian.org/volian/ scar main" | sudo tee -a "/etc/apt/sources.list.d/volian-archive-scar-unstable.list"
